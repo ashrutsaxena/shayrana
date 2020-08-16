da0315f7da768276f62f9b647bd4849f1c8f6b23
ashrut saxena <ashrut.saxena_cs18@gla.ac.in>
2020-08-16 18:56:01 +0700

    animation

--- app/src/main/java/ashrut/saxena/destrution/shayrana/MainActivity.java
+++ app/src/main/java/ashrut/saxena/destrution/shayrana/MainActivity.java
@@ -1,14 +1,17 @@
 package ashrut.saxena.destrution.shayrana;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }
