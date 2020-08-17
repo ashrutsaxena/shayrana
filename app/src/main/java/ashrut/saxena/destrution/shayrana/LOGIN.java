9eca1c293f6daec90fad885542b19c4909aa848d
ashrut saxena <ashrut.saxena_cs18@gla.ac.in>
2020-08-17 18:27:20 +0700

    animation2

---
+++ app/src/main/java/ashrut/saxena/destrution/shayrana/LOGIN.java
@@ -0,0 +1,14 @@
+package ashrut.saxena.destrution.shayrana;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LOGIN extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/ashrut/saxena/destrution/shayrana/MainActivity.java
+++ app/src/main/java/ashrut/saxena/destrution/shayrana/MainActivity.java
@@ -1,11 +1,24 @@
 package ashrut.saxena.destrution.shayrana;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    //  SPLASH SCREEN TIME
+    private static int SPLASH_SCREEN = 5000;
+
+    //VARIABLES
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, slogan;
 
 
     @Override
@@ -13,5 +26,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        //ANIMATIONS
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //ATTACH
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.txtappname);
+        slogan = findViewById(R.id.txtwrite);
+
+        //ANIM
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        //NEXT ACTIVITY
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LOGIN.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
     }
 }
